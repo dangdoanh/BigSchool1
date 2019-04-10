2dd13fc6424d0aea9c43b7804837b9323639a4a5
BlackZ <anhtraicntt@gmail.com>
2019-04-10 21:36:06 +0700

    Add project files.

---
+++ MyBigSchool/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using MyBigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using MyBigSchool.ViewModels;
+
+namespace MyBigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        //private readonly string c;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult Index()
+        {
+            var upcommingCourses = _dbContext.Courses
+
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            var viewModel = new CoursesViewModel
+            {
+                UpCommingCourses = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
